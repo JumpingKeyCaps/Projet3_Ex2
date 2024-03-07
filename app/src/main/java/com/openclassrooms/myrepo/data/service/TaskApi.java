25861f15f8c4c136a26bbf1328b669fd993fd4e8
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-07 02:58:25 +0100

    Ajout de la methode formatDueTimeToShow() pour formater le temps limite en une String utilisable par le TextView associer. Ajout de la methode calculateDueTimeProgress pour determiner la valeur de la progressbar en pourcentage. modification mineur du layout task_item.xml

--- app/src/main/java/com/openclassrooms/myrepo/data/service/TaskApi.java
+++ app/src/main/java/com/openclassrooms/myrepo/data/service/TaskApi.java
@@ -25,31 +25,56 @@ public class TaskApi {
         // Simule la récupération des tâches depuis une API
         List<Task> tasks = new ArrayList<>();
 
-        //on recupere une instance de Calendar
-        Calendar calendar = Calendar.getInstance();
-        //on lui definie la date actuel
-        calendar.setTime(new Date());
-        // Pour chaque task que l'on cree, on ajoute le nombre de jour par default a notre objet calendar et on passe la date retourner via .getTime() en parametre au constructor de la task a creer.
-        tasks.add(new Task("Faire les courses pour le dîner",calendar.getTime()));
 
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Préparer le rapport pour la réunion",calendar.getTime()));
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Répondre aux e-mails en attente",calendar.getTime()));
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Faire de l'exercice pendant 30 minutes",calendar.getTime()));
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Planifier les vacances d'été",calendar.getTime()));
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Rendre le livre à la bibliothèque",calendar.getTime()));
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Réviser pour l'examen de mathématiques",calendar.getTime()));
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Appeler le plombier pour la fuite d'eau",calendar.getTime()));
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Nettoyer le garage",calendar.getTime()));
-        calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
-        tasks.add(new Task("Préparer une liste de courses",calendar.getTime()));
+        // Pour chaque task que l'on on genere une date limite via la methode fakeTaskGenerator
+
+        //on genere des taches avec des temps limite personaliser
+        tasks.add(new Task("Faire les courses pour le dîner",fakeDueTimeTaskGenerator(false,1)));
+        tasks.add(new Task("Préparer le rapport pour la réunion",fakeDueTimeTaskGenerator(false,2)));
+        tasks.add(new Task("Répondre aux e-mails en attente",fakeDueTimeTaskGenerator(false,3)));
+        tasks.add(new Task("Faire de l'exercice pendant 30 minutes",fakeDueTimeTaskGenerator(false,5)));
+        tasks.add(new Task("Planifier les vacances d'été",fakeDueTimeTaskGenerator(false,8)));
+
+        //on genere des taches avec des temps limite  par default
+        tasks.add(new Task("Rendre le livre à la bibliothèque",fakeDueTimeTaskGenerator(true,null)));
+        tasks.add(new Task("Réviser pour l'examen de mathématiques",fakeDueTimeTaskGenerator(true,null)));
+        tasks.add(new Task("Appeler le plombier pour la fuite d'eau",fakeDueTimeTaskGenerator(true,null)));
+        tasks.add(new Task("Nettoyer le garage",fakeDueTimeTaskGenerator(true,null)));
+
+        //on simule une erreur de parametre en utilisant la methode
+        tasks.add(new Task("Préparer une liste de courses",fakeDueTimeTaskGenerator(false,null)));
         return tasks;
     }
+
+    /**
+     * Generateur de date limite pour simuler un comportement par default ou personaliser
+     *
+     * @param isDefaultDueTime  permet de determiner si on applique une date limite par default a la tache (true) ou une date custom (false).
+     * @param dueTimeInDays permet dans le cas ou isDefaultDueTime est false, de fixer une date limite en nombre de jour.
+     *                      (on utilise une Integer plutot q'un int car Integer peut etre null)
+     * @return un objet Date  representant notre date limite pour la tache
+     */
+    private Date fakeDueTimeTaskGenerator(boolean isDefaultDueTime, Integer dueTimeInDays){
+        //on recupere une instance de Calendar
+        Calendar calendar = Calendar.getInstance();
+        //on reset notre calendar
+        calendar.clear();
+        //on lui definie la date actuel (on simule une tache qui a ete creer aujourdhui)
+        calendar.setTime(new Date());
+        //discrimination du mode default vs custom
+        if(isDefaultDueTime){
+            // valeur par default (10 jours))
+            calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
+        }else{
+            // valeur personaliser (on securise avec un block Try{}catch en cas d'erreur d'utilisation de la methode)
+            try{
+                calendar.add(Calendar.DAY_OF_YEAR, dueTimeInDays);
+            }catch (NullPointerException e){
+                //on fix la valeur par default
+                calendar.add(Calendar.DAY_OF_YEAR, DEFAULT_DATE_LIMITE);
+            }
+        }
+        return calendar.getTime();
+    }
+
 }
--- app/src/main/java/com/openclassrooms/myrepo/ui/TaskRecyclerViewAdapter.java
+++ app/src/main/java/com/openclassrooms/myrepo/ui/TaskRecyclerViewAdapter.java
@@ -8,8 +8,14 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.openclassrooms.myrepo.R;
 import com.openclassrooms.myrepo.model.Task;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Un adaptateur pour afficher la liste de tâches dans un RecyclerView.
@@ -34,20 +40,27 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.bind(getItem(position));
     }
-
+//
     /**
      * ViewHolder pour afficher les éléments de la liste de tâches.
      */
     static class ViewHolder extends RecyclerView.ViewHolder {
 
         private final TextView factTextView;
+        private final TextView dueTimeTextView;
+        private final LinearProgressIndicator dueTimeProgressIndicator;
 
         /**
          * Constructeur du ViewHolder.
+         *
+         * @param itemView la View racine du layout inflater utiliser pour representer une ligne du RecyclerView.
          */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            // on recupere nos widgets via notre View racines passer en parametre du constructeur (itemView) dans laquelle a ete inflater notre layout task_item.xml
             factTextView = itemView.findViewById(R.id.task_description);
+            dueTimeTextView = itemView.findViewById(R.id.task_duetime);
+            dueTimeProgressIndicator = itemView.findViewById(R.id.task_duetime_progress_horizontal);
         }
 
         /**
@@ -57,7 +70,54 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
          */
         public void bind(Task task) {
             factTextView.setText(task.getDescription());
+            //on formate la date limite via une methode dedier pour l'afficher dans son textView
+            dueTimeTextView.setText(formatDueTimeToShow(task.getDueTime()));
+            //on set la valeur de temps restant de notre ProgressIndicator via la methode de calcule dedier.
+            dueTimeProgressIndicator.setProgress(calculateDueTimeProgress(task.getDueTime()));
         }
+
+        /**
+         * Formate notre temps limite en jours/mois/annees pour etre directement utiliser par son textview associer.
+         *
+         * @param taskDueTime La date limite de la tache a formater.
+         * @return une String  representant la date limite dans un format dd/MM/yyyy  preceder d'un text informatif static
+         */
+        public String formatDueTimeToShow(Date taskDueTime){
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+            return "Date limite : "+dateFormat.format(taskDueTime);
+        }
+
+        /**
+         * Calcule la valeur de progression (en %) de la barre de progression de la date limite.
+         *
+         * @param dueTime La date limite de la tâche.
+         * @return La progression actuel en pourcentage.
+         */
+        private int calculateDueTimeProgress(Date dueTime) {
+
+            //La date actuel
+            Calendar currentDayCalendar = Calendar.getInstance();
+            currentDayCalendar.set(Calendar.HOUR_OF_DAY, 0);
+            currentDayCalendar.set(Calendar.MINUTE, 0);
+            currentDayCalendar.set(Calendar.SECOND, 0);
+            currentDayCalendar.set(Calendar.MILLISECOND, 0);
+
+            // La date limite de la task
+            Calendar DueTimeCalendar = Calendar.getInstance();
+            DueTimeCalendar.setTime(dueTime);
+            DueTimeCalendar.set(Calendar.HOUR_OF_DAY, 0);
+            DueTimeCalendar.set(Calendar.MINUTE, 0);
+            DueTimeCalendar.set(Calendar.SECOND, 0);
+            DueTimeCalendar.set(Calendar.MILLISECOND, 0);
+
+            //Calcule de la difference entre la date limite et la date actuel
+            int daysRemaining = Math.toIntExact((DueTimeCalendar.getTimeInMillis() - currentDayCalendar.getTimeInMillis()) / (24 * 60 * 60 * 1000));
+            //on retunr
+            return 100 - (daysRemaining * 10);
+
+        }
+
+
     }
 
     /**
